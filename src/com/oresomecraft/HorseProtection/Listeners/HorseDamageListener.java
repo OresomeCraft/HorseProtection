19d8ed4bb857616020396210769c3975a026fd83
Zach De Koning <zach.de.koning@gmail.com>
2013-07-18 17:39:17 +1000

    Use EntityDamageByEntityEvent instead

--- src/com/oresomecraft/HorseProtection/Listeners/HorseDamageListener.java
+++ src/com/oresomecraft/HorseProtection/Listeners/HorseDamageListener.java
@@ -7,7 +7,7 @@ import org.bukkit.entity.EntityType;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
-import org.bukkit.event.entity.EntityDamageEvent;
+import org.bukkit.event.entity.EntityDamageByEntityEvent;
 
 public class HorseDamageListener implements Listener {
 
@@ -18,13 +18,13 @@ public class HorseDamageListener implements Listener {
     }
 
     @EventHandler
-    public void onEntityDamage(EntityDamageEvent event) {
+    public void onEntityDamage(EntityDamageByEntityEvent event) {
         if (event.getEntityType() == EntityType.HORSE) {
             if (plugin.getConfig().getString(event.getEntity().getUniqueId().toString() + ".Owner") != null) {
                 event.setCancelled(true);
                 for (Player p : Bukkit.getOnlinePlayers()) {
-                    if (p.hasPermission("HorseProtection.moderator") && (event.getEntity() instanceof Player)) {
-                        p.sendMessage(ChatColor.RED + "User" + ChatColor.DARK_RED + ((Player) event.getEntity()).getName()
+                    if (p.hasPermission("HorseProtection.moderator") && (event.getDamager() instanceof Player)) {
+                        p.sendMessage(ChatColor.RED + "User" + ChatColor.DARK_RED + ((Player) event.getDamager()).getName()
                                 + ChatColor.RED + " tried to damage a horse belonging to "
                                 + plugin.getConfig().getString(event.getEntity().getUniqueId().toString() + ".Owner"));
 
