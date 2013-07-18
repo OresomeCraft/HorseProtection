a5fd8751aaabad2744543db26336cc7d925e4559
Andrew Wickens <andrewjwickens@gmail.com>
2013-07-18 17:52:51 +1000

    horses now not completely invulnerable

--- src/com/oresomecraft/HorseProtection/Listeners/HorseDamageListener.java
+++ src/com/oresomecraft/HorseProtection/Listeners/HorseDamageListener.java
@@ -8,6 +8,7 @@ import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.EntityDamageByEntityEvent;
+import org.bukkit.event.entity.EntityDamageEvent.DamageCause;
 
 public class HorseDamageListener implements Listener {
 
@@ -21,16 +22,30 @@ public class HorseDamageListener implements Listener {
     public void onEntityDamage(EntityDamageByEntityEvent event) {
         if (event.getEntityType() == EntityType.HORSE) {
             if (plugin.getConfig().getString(event.getEntity().getUniqueId().toString() + ".Owner") != null) {
-                event.setCancelled(true);
-                for (Player p : Bukkit.getOnlinePlayers()) {
-                    if (p.hasPermission("HorseProtection.moderator") && (event.getDamager() instanceof Player)) {
-                        p.sendMessage(ChatColor.RED + "User" + ChatColor.DARK_RED + ((Player) event.getDamager()).getName()
-                                + ChatColor.RED + " tried to damage a horse belonging to "
-                                + plugin.getConfig().getString(event.getEntity().getUniqueId().toString() + ".Owner"));
-
-                    }
-                }
+	        	if (event.getDamager() instanceof Player) {
+		                event.setCancelled(true);
+		                for (Player p : Bukkit.getOnlinePlayers()) {
+		                    if (p.hasPermission("HorseProtection.moderator")) {
+		                        p.sendMessage(ChatColor.RED + "User" + ChatColor.DARK_RED + ((Player) event.getDamager()).getName()
+		                                + ChatColor.RED + " tried to damage a horse belonging to "
+		                                + plugin.getConfig().getString(event.getEntity().getUniqueId().toString() + ".Owner"));
+		
+		                    }
+		                }
+	        	} else if (invalidDamageCause(event.getCause())) {
+	        		event.setCancelled(true);
+	        	}
             }
         }
     }
+
+	private boolean invalidDamageCause(DamageCause cause) {
+		switch (cause) {
+		case ENTITY_ATTACK:
+		case PROJECTILE:
+		case MAGIC: 
+		case POISON: return true;
+		}
+		return false;
+	}
 }
